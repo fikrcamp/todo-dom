586993b99efc7d056099266784b24345dd035219
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2022-11-19 21:15:46 +0300

    solved

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello");
+document.querySelector("#add").addEventListener("click",function(){
+    let task = document.querySelector("#input").value
+    let div = document.createElement("div")
+    div.className = "new"
+    div.innerText = task
+    document.querySelector(".tasks").appendChild(div)
+    document.querySelector(".new").classList.add("tasks", "task")
+})
