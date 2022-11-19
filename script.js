2bfad847f0665f677b04315abbd3f2f274cf828d
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2022-11-19 21:23:13 +0300

    minor edits

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.querySelector("#add").addEventListener("click",function(){
-    let task = document.querySelector("#input").value
+    let todo = document.querySelector("#input").value
     let div = document.createElement("div")
-    div.className = "new"
-    div.innerText = task
+    div.className = "task"
+    div.innerText = todo
     document.querySelector(".tasks").appendChild(div)
-    document.querySelector(".new").classList.add("tasks", "task")
+    document.querySelector(".task").classList.add("tasks")
 })
