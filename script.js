22e00d22e8a56b2c5e356f9797f8d245a47581f3
zayid <sayidka843@gmail.com>
2022-11-20 22:29:50 +0600

    solved the todo-dom exercise

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+ 
+
+let add =document.querySelector("#add");
+
+add.addEventListener("click" ,function(){
+    let list= document.createElement("div");
+    let input =document.querySelector("#input").value;
+    const task =document.querySelector("#task");
+    list.innerText= input;
+    list.classList= "task";
+    task.appendChild(list);
+    
+})
