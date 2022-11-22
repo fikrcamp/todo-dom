1d574609926a95b633a4fccac1328e1f7304498a
Abdilahi mohamed <cabdale3377@gmail.com>
2022-11-22 11:12:27 +0300

    todo app using javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+
+  document.querySelector("#add").addEventListener("click", function(){
+    let task =  document.querySelector("#input").value;
+
+    let newTask = document.createElement("div"); 
+    newTask.innerText = task;
+
+    document.querySelector(".tasks").appendChild(newTask);
+
+    // console.log(newTask);
+})
