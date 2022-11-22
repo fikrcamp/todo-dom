4678b8f1c28645e07a04a1271400354f37d81e32
AbduleYousuf <abduleyousuf08@gmail.com>
2022-11-22 09:32:54 +0300

    finnished To-do list DOM

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+//javascript
+
+//making the process of click
+document.querySelector("#add").addEventListener("click", function () {
+    //storing a value from my input
+    let value = document.querySelector("#input").value
+    //making a new element "Div"
+    let element = document.createElement("div")
+    //giving a value my new element or innerText with accessing my value from my input
+    element.innerText = value
+    //to indecate where the new div will be added or his place
+    document.querySelector(".tasks").appendChild(element).classList.add("task")
+})
