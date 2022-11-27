fabbeb8f27eb7aed2e099bdc66561ada7b8bcf5c
Abdalla <115536293+Abdalla-10@users.noreply.github.com>
2022-11-27 22:04:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,23 +19,3 @@ document.querySelector("#add").addEventListener("click", function(){
 
 
 
-// <!-- <ul id="students">
-// <li>Ahmed</li>
-// </ul> -->
-
-
-
-
-// document.querySelector("#add").addEventListener("click",function(){
-//     //1. get the name from the textbox
-//     let name = document.querySelector("#input").value
-    
-//     // 2. create a new li element   li = <li></li>
-//     let li = document.createElement("li")
-    
-//     //3. inside li text name   <li>Ali</li>
-//     li.innerText = name 
-    
-//     //4. indicat where to put the new li element
-//     document.querySelector("#students").appendChild(li)
-// })
